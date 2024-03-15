7e6a9725a743b51144d853e421a49aa7c431d0e8
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-15 21:51:43 +0500

    Initial Setrup

---
+++ Travling/src/pages/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
+
+const Layout = () => {
+  return (
+    <>
+        <Navbar />
+        <Footer />
+    </>
+  )
+}
+
+export default Layout
