95ecc2f170dba058870a05920a6546845c799e60
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-15 22:19:59 +0500

    Layout and Routes settings

--- Travling/src/App.jsx
+++ Travling/src/App.jsx
@@ -1,11 +1,27 @@
-import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blogs from "./pages/Blogs";
+import PlacesRoute from "./pages/PlacesRoute";
+import BlogsDetails from "./pages/BlogsDetails";
+import NoPage from "./pages/NoPage";
 
 export default function App() {
   return (
-   <>
-    <div>
-      <Navbar />
-    </div>
-   </>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/blogs/:id" element={<BlogsDetails />} />
+            <Route path="/places" element={<PlacesRoute />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
--- Travling/src/pages/Layout.jsx
+++ Travling/src/pages/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <>
         <Navbar />
+        <Outlet />
         <Footer />
     </>
   )
