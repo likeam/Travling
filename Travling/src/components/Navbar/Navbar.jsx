d498b1d00f4c3dc5df0c7f22c1cc78c23b5ec28f
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-16 09:54:04 +0500

    Building Navbar Section

--- Travling/src/components/Navbar/Navbar.jsx
+++ Travling/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../../assets/logo.jpg";
+import { FaCaretDown } from "react-icons/fa";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const dropdownLinks = [
+    {
+      name: "Our Services",
+      link: "/services",
+    },
+    {
+      name: "Top Brands",
+      link: "/#mobile_brands",
+    },
+    {
+      name: "Location",
+      link: "/#location",
+    },
+  ];
 
-export default Navbar
+  return (
+    <>
+      <div className=" fixed top-0 right-0 w-full bg-white text-black shadow-md">
+        <div className="bg-gradient-to-r from-primary to-secondary text-white">
+          <div className=" container py-[2px] sm:block hidden ">
+            <div className="flex">
+              <p>20% off on next booking</p>
+              <p>Mobile No. +92-300-6644123</p>
+            </div>
+          </div>
+        </div>
+        <div className=" container py-3 sm:py-0">
+          <div className="flex justify-between">
+            <div>
+              <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+                <img src={Logo} alt="logo" className=" h-16" />
+              </Link>
+            </div>
+            <div className=" hidden md:block">
+              <ul className="flex items-center gap-6">
+                <li className="py-4">
+                  <NavLink
+                    activeclassname="active"
+                    to="/"
+                    onClick={() => window.scrollTo(0, 0)}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="py-4">
+                  <NavLink
+                   activeclassname="active"
+                    to="/blogs"
+                    onClick={() => window.scrollTo(0, 0)}
+                  >
+                    Blogs
+                  </NavLink>
+                </li>
+                <li className="py-4">
+                  <NavLink
+                   activeclassname="active"
+                    to="/places"
+                    onClick={() => window.scrollTo(0, 0)}
+                  >
+                    Places
+                  </NavLink>
+                </li>
+                <li className="py-4">
+                  <NavLink
+                    activeclassname="active"
+                    to="/about"
+                    onClick={() => window.scrollTo(0, 0)}
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li className=" py-4 relative group cursor-pointer">
+                  <div className=" dropdown flex items-center">
+                    <span>Quick Links</span>
+                    <span>
+                      <FaCaretDown className=" transition-all duration-200 group-hover:rotate-180" />
+                    </span>
+                  </div>
+                  <div className=" absolute -left-9 z-[9999] hidden group-hover:block shadow-md text-black w-[150px] bg-white p-2">
+                    <ul>
+                      {dropdownLinks.map(({ name, link }) => {
+                        return(
+                          <li key={name}>
+                          <a
+                            className=" inline-block w-full rounded-md p-2 hover:bg-primary/20"
+                            href={link}
+                          >
+                            {name}
+                          </a>
+                        </li>
+                        )
+                      })}
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <button className=" bg-gradient-to-r from-primary to-secondary hover: bg-bg-gradient-to-r hover:from-secondary hover:bg-primary transition-all duration-500 text-white px-3 py-1 rounded-full">Book Now</button>
+              <div className=" md:hidden block">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
